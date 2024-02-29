02a8e07d4cc1b6eda340c6bfcec69f7671bdcde0
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-02-29 15:22:22 +0500

    Updated Code

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,8 +8,8 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index(){
-        $post = Post::all()->last();
-        dd($post);
+        $post = Post::get(); // or $post = Post::all();
+        //dd($post); //dum death
         return view('welcome', ['post'=>$post]);
     }
 
