b4b37782b18962d4fbe7b00184b7cff740cf4a3e
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-02-29 13:23:01 +0500

    addl records to the database with one line command

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    //Here we can add these all record to the database with one line command
+    //Post::create(['title'=>'third title','excerpt'=>'third long excerpt','content'=>'third long content','image'=>'images/tech3.jpg']);
+    protected $fillable = [
+        'title',
+        'excerpt',
+        'content',
+        'image'
+    ];
+
+    //Here we can not add id
+    protected $guarded = [
+        'id'
+    ];
 }
